9f548766650407d641ef3e972df1204b9023962f
Sahil Bhatia <ssb@Apples-MacBook-Pro.local>
2025-08-07 13:07:22 +0530

    added failing tests for add

---
+++ calculator.py
@@ -0,0 +1,9 @@
+'''
+TDD:
+Failing test first
+Writing that make test pass
+Refactor to improve it
+'''
+
+def add(num_string):
+    pass
---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+'''
+TDD:
+Failing test first
+Writing that make test pass
+Refactor to improve it
+'''
+from calculator import add
+
+def test_empty_string_returns_zero():
+    assert add("") == 0
+
+def test_single_num_string_returns_num():
+    assert add("1") == 1
+
+def test_string_with_whitespace_delimiter():
+    assert add("1 2") == 3
